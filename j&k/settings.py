293b429538b1d46697991a1657a580162082b37a
Mr Shah <vikasshah8218@gmail.com>
2025-02-26 22:58:29 +0530

    ok

--- j&k/settings.py
+++ j&k/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-0l@^u^h%o3dz^xz#r2*#86qqt(pj)k2+o)(1i&tlurwq97ma'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["*",'ess-be.techkingdom.in']
 
 
 # Application definition
